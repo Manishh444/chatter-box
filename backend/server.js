71f226af2070f97d453f979dc9e9a30f261d4b25
Manish Bhardwaj <manishbhardwaj131149@gmail.com>
2023-01-01 12:41:02 +0530

    Get api v1

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const express  = require('express');
-
+const express = require("express");
+const { chats } = require("./data/data");
 const app = express();
 
-app.get('/',(req, res)=>{
-    res.send("Welcome to Chatter-box")
-})
-
-app.listen(5000, console.log("Server at 5000"))
+app.get("/", (req, res) => {
+  res.send("Welcome to Chatter-box");
+});
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+app.listen(5000, console.log("Server at 5000"));
