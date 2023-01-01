d2e36e038241cfb4c8ea53d38c8d791fab639eba
Manish Bhardwaj <manishbhardwaj131149@gmail.com>
2023-01-01 13:20:49 +0530

    GET API for single id

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,13 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Welcome to Chatter-box");
 });
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats);
+// });
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+    console.log(req.params.id)
 });
 app.listen(5000, console.log("Server at 5000"));
