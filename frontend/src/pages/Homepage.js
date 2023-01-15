cb4d76b5538d6b8304d7553f3d28f6e8e58d119d
Manish Bhardwaj <manishbhardwaj131149@gmail.com>
2023-01-16 00:13:50 +0530

    HomePage continue.

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,9 +1,49 @@
-import React from 'react'
-
+import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
 const Homepage = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        display={"flex"}
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius={"md"}
+        borderWidth={"1px"}
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          Bol-Bacchan
+        </Text>
+      </Box>
+      <Box bg={"white"} w="100%" p="4" borderRadius={"lg"} borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb={"1em"}>
+            <Tab width={"50%"}>Login</Tab>
+            <Tab width={"50%"}>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login></Login>
+            </TabPanel>
+            <TabPanel>
+              <SignUp></SignUp>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default Homepage
+export default Homepage;
